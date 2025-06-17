712333c53cb4fc4849c1f2f3a518c8b4cf9c4c6f
Thomas Newman <tnewman057@gmail.com>
2025-06-17 11:31:07 -0400

    just rulebased chatbot for day 2

---
+++ Day 1 Period 2/denarytobinary.py
@@ -0,0 +1,3 @@
+num = input("Enter a denary(decimal) number: ")
+binary_num = bin(int(num))[2:]
+print(f"The binary equivalent of {num} is {binary_num}.")
---
+++ Day 1 Period 2/pyramid.py
@@ -0,0 +1,5 @@
+height = int(input("Enter the height of the pyramid: "))
+
+for i in range(1, height + 1):
+    print(" " * (height - i) + "*" * (2 * i - 1))
+    
